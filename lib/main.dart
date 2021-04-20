6f9ae4e8317a07a32b1133c7b9602a1bc0c4fd7b
Marcondes <mvm.sp11@gmail.com>
2021-04-20 15:25:26 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-// Copyright 2020 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,20 +12,14 @@ void main() => runApp(MyAdaptingApp());
 class MyAdaptingApp extends StatelessWidget {
   @override
   Widget build(context) {
-    // Either Material or Cupertino widgets work in either Material or Cupertino
-    // Apps.
     return MaterialApp(
       title: 'Adaptive Music App',
       theme: ThemeData(
-        // Use the green theme for Material widgets.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       darkTheme: ThemeData.dark(),
       builder: (context, child) {
         return CupertinoTheme(
-          // Instead of letting Cupertino widgets auto-adapt to the Material
-          // theme (which is green), this app will use a different theme
-          // for Cupertino (which is blue by default).
           data: CupertinoThemeData(),
           child: Material(child: child),
         );
@@ -40,12 +30,6 @@ class MyAdaptingApp extends StatelessWidget {
 }
 
 // Shows a different type of scaffold depending on the platform.
-//
-// This file has the most amount of non-sharable code since it behaves the most
-// differently between the platforms.
-//
-// These differences are also subjective and have more than one 'right' answer
-// depending on the app and content.
 class PlatformAdaptingHomePage extends StatefulWidget {
   @override
   _PlatformAdaptingHomePageState createState() =>
@@ -53,17 +37,8 @@ class PlatformAdaptingHomePage extends StatefulWidget {
 }
 
 class _PlatformAdaptingHomePageState extends State<PlatformAdaptingHomePage> {
-  // This app keeps a global key for the songs tab because it owns a bunch of
-  // data. Since changing platform re-parents those tabs into different
-  // scaffolds, keeping a global key to it lets this app keep that tab's data as
-  // the platform toggles.
-  //
-  // This isn't needed for apps that doesn't toggle platforms while running.
   final songsTabKey = GlobalKey();
 
-  // In Material, this app uses the hamburger menu paradigm and flatly lists
-  // all 4 possible tabs. This drawer is injected into the songs tab which is
-  // actually building the scaffold around the drawer.
   Widget _buildAndroidHomePage(BuildContext context) {
     return SongsTab(
       key: songsTabKey,
@@ -71,14 +46,6 @@ class _PlatformAdaptingHomePageState extends State<PlatformAdaptingHomePage> {
     );
   }
 
-  // On iOS, the app uses a bottom tab paradigm. Here, each tab view sits inside
-  // a tab in the tab scaffold. The tab scaffold also positions the tab bar
-  // in a row at the bottom.
-  //
-  // An important thing to note is that while a Material Drawer can display a
-  // large number of items, a tab bar cannot. To illustrate one way of adjusting
-  // for this, the app folds its fourth tab (the settings page) into the
-  // third tab. This is a common pattern on iOS.
   Widget _buildIosHomePage(BuildContext context) {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
